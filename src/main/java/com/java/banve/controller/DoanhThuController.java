684b088bb5067af892a2aad1464fa5cbc8ac36c9
tutranvan156 <tutranvan156@gmail.com>
2021-06-22 07:48:21 +0700

    cơ bản xong chưa xử lí chart

--- src/main/java/com/java/banve/controller/DoanhThuController.java
+++ src/main/java/com/java/banve/controller/DoanhThuController.java
@@ -1,18 +1,42 @@
 package com.java.banve.controller;
 
+import com.java.banve.model.ChartData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.*;
+
 @Controller
 @CrossOrigin
 @RequestMapping("/doanh-thu")
+
 public class DoanhThuController {
 
 
-    @RequestMapping("")
-    public String doanhthu() {
-        return "doanhthu";
+   @RequestMapping("")
+   public String test() {
+       return "test";
+   }
+    @RequestMapping("/a")
+    public ResponseEntity<?> doanhthu() {
+
+        ChartData chartData = new ChartData();
+        Map<Character, List<Integer>> mappedData = new HashMap<>();
+        String s = "abc";
+        for (int i = 0; i < 3; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < 20; j++) {
+                Random random = new Random();
+                int value = random.nextInt(20) + 1;
+                temp.add(value);
+            }
+            mappedData.put(s.charAt(i), temp);
+        }
+
+        return new ResponseEntity<>(mappedData, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/java/banve/model/ChartData.java
@@ -0,0 +1,14 @@
+package com.java.banve.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ChartData {
+    public Integer id;
+    public String name;
+    public Integer value;
+
+}
